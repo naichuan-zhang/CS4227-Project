262757cb69173496d4c548f4ac211296ebc6d10d
Naichuan Zhang <zhangnaichuan168@gmail.com>
2020-04-08 14:19:28 +0100

    coverage test reports

--- order/interceptor/context.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-from order.models import Order
-
-
-# class OrderContext(ABC):
-#     def __init__(self, order: Order):
-#         super(OrderContext, self).__init__(order)
-#
