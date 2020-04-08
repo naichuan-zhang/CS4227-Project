8f029e02631e0019d2140385ffc9b782f072ede3
nealecon96 <32375299+nealecon96@users.noreply.github.com>
2020-04-08 16:31:18 +0100

    updating payment
    to be completed

--- static/app/main/js/view_order.js
+++ static/app/main/js/view_order.js
@@ -3,3 +3,16 @@ function make_payment(id){
             window.location = "/order/previous/view/make_payment";
         };
 
+function show(x, y){
+        if(document.getElementById(x).style.display == "none" && document.getElementById(y).style.display != "none"){
+            document.getElementById(x).style.display = "block";
+            document.getElementById(y).style.display = "none";
+        }
+        else if(document.getElementById(x).style.display == "none") {
+            document.getElementById(x).style.display = "block";
+        }
+        else{
+            document.getElementById(x).style.display = "none";
+        }
+    };
+
